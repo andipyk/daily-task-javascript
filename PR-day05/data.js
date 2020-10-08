8aa9adc5379d0eb75e29014d7cfd8ec685d1381e
Andi Syafrianda <andi.syafrianda@aux.go-jek.com>
2020-10-09 06:01:18 +0700

    progress

---
+++ PR-day05/data.js
@@ -0,0 +1,26 @@
+let database = [
+    {
+        "userId":"rirani",
+        "roleId":"hrd",
+        "firstName":"Romin",
+        "lastName":"Irani",
+        "preferredFullName":"Romin Irani",
+        "employeeCode":"E1",
+        "address":"Jakarta",
+        "phoneNumber":"408-1234567",
+        "emailAddress":"romin.k.irani@gmail.com",
+        "password": "1234"
+    },
+    {
+        "userId":"nirani",
+        "roleId":"karyawan",
+        "firstName":"Neil",
+        "lastName":"Irani",
+        "preferredFullName":"Neil Irani",
+        "employeeCode":"E2",
+        "address":"Payakumbuh",
+        "phoneNumber":"408-1111111",
+        "emailAddress":"neilrirani@gmail.com",
+        "password": "4321"
+    }
+]
---
+++ PR-day05/script.js
@@ -0,0 +1,24 @@
+const registerSave = () => {
+    const newData = inputCollector("group-register")
+    if (isUnique(newData.userId)){
+        database.push(newData)
+        alert("data succesfully added !")
+    } else {
+        alert("userid sudah digunakan !")
+    }
+}
+
+//check username & password isMatch return data
+const authLogin = (userName, userPass) => {
+    const dataKaryawan = database.filter(employee => employee.userId === userName && employee.password === userPass)
+    dataKaryawan.length === 0 ? alert("username/password salah") : alert("berhasil login")
+
+    return dataKaryawan
+}
+
+
+
+
+// const newArry = database.map((employee) => employee.roleId)
+//
+// console.log(newArry)
