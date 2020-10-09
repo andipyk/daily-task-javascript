54af44fb7ff8761b88f96a9cbf410721ce6d8f56
Andi Syafrianda <andi.syafrianda@aux.go-jek.com>
2020-10-09 07:20:21 +0700

    dinamyc table

--- PR-day05/helper.js
+++ PR-day05/helper.js
@@ -25,7 +25,5 @@ const isUnique = newUserId  => {
 //check username & password isMatch return data
 const authLogin = (userName, userPass) => {
     const dataKaryawan = database.filter(employee => employee.userId === userName && employee.password === userPass)
-    // dataKaryawan.length === 0 ? alert("username/password salah") : alert("berhasil login")
-
     return dataKaryawan
 }
--- PR-day05/script.js
+++ PR-day05/script.js
@@ -1,7 +1,7 @@
 // when button register for save data to database
 const registerSave = () => {
     const newData = inputCollector("group-register")
-    if (isUnique(newData.userId)){
+    if (isUnique(newData.userId)) {
         database.push(newData)
         alert("data succesfully added !")
     } else {
@@ -12,7 +12,7 @@ const registerSave = () => {
 
 const loginBtn = () => {
     const loginData = inputCollector("group-login")
-    if (authLogin(loginData.userId, loginData.password).length !== 0 ) {
+    if (authLogin(loginData.userId, loginData.password).length !== 0) {
         alert("login berhasil !")
 
     } else {
@@ -21,3 +21,27 @@ const loginBtn = () => {
     }
 
 }
+
+// Create table with data
+const createTable = () => {
+
+    const tablearea = document.getElementById('myTable')
+    const table = document.createElement('table');
+
+
+    for (var i = 0; i < database.length; i++) {
+        const tr = document.createElement('tr');
+        const databaseKeys = Object.keys(...database)
+
+        for (let j = 0; j < databaseKeys.length; j++) {
+            tr.appendChild(document.createElement('td'));
+            tr.cells[j].appendChild(document.createTextNode(`${database[i][databaseKeys[j]]}`))
+        }
+        table.appendChild(tr);
+    }
+
+    tablearea.replaceChild(table , tablearea.childNodes[0]);
+}
+
+
+
